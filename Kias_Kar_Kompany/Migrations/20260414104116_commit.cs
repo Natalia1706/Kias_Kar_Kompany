0476e363b32d065fed8534f59f58c54f3a7ab7b4
Natalia1706 <160506115+Natalia1706@users.noreply.github.com>
2026-04-14 12:41:51 +0200

    added migration

---
+++ Kias_Kar_Kompany/Migrations/20260414104116_commit.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kias_Kar_Kompany.Migrations
+{
+    /// <inheritdoc />
+    public partial class commit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "manId",
+                table: "Vehicle",
+                newName: "ManId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ManId",
+                table: "Vehicle",
+                newName: "manId");
+        }
+    }
+}
